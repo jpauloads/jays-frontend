7ed6710a13070957679647fc2cf765a021dc6cc1
jpauloads <102815481+jpauloads@users.noreply.github.com>
2023-10-02 19:54:49 -0300

    Validações do formulario

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -8,66 +8,107 @@ import { useCallback, useEffect } from "react";
 import axios from 'axios';
 import { zipCodeMask } from "../utils/masks";
 
-const schemaForm = z.object({
+const createUserFormSchema = z.object({
+    name: z.string().min(1, 'Nome é necessário'),
+    lastName: z.string().min(1, 'Sobrenome é necessário'),
+    email: z.string().email('Email inválido'),
+    password: z.string().min(8, 'Senha deve ter pelo menos 8 caracteres'),
+    confirmPassword: z.string().min(8, 'Confirmação de senha deve ter pelo menos 8 caracteres'),
+    phone: z.string().min(9, 'Número inválido'),
+    birthdate: z.string().min(1,'Informe a data'),
+    username: z.string().min(4, 'Username deve ter pelo menos 4 caracteres'),
+    cpf: z.string().length(11, 'CPF inválido'),
     address: z.object({
         zipCode: z.string().min(9, 'Por favor, informe um CEP válido'),
         street: z.string().min(1, 'Por favor, uma rua válida'),
-        number: z.string().min(1, 'Campo inválido'),
+        number: z.string(),
         city: z.string().min(1, 'Por favor, informe uma cidade válida'),
         state: z.string().min(1, 'Campo inválido'),
+        district: z.string().min(1, 'Campo inválido'),
     })
 })
 .transform((field) => ({
+    name: field.name,
+    lastName: field.lastName,
+    email: field.email,
+    password: field.password,
+    confirmPassword: field.confirmPassword,
+    phone: field.phone,
+    birthdate: field.birthdate,
+    username: field.username,
+    cpf: field.cpf,
     address: {
         zipCode: field.address.zipCode,
         street: field.address.street,
         number: field.address.number,
         city: field.address.city,
         state: field.address.state,
+        district: field.address.district
     }
 }));
 
-const styleP = "text-sm font-semibold text-red-500"
+const styleP = "text-xs font-semibold text-red-500"
 
-type FormProps = z.infer<typeof schemaForm>
+type CreateUserFormData = z.infer<typeof createUserFormSchema>
 type AddressProps = {
     logradouro: string;
     localidade: string;
     uf: string;
+    bairro: string;
 }
 
 export function RegisterPage() {
+
     const { 
-        handleSubmit, 
+        handleSubmit,
         register, 
         watch,
         setValue,
+        setError,
         formState: { errors }
-    } = useForm<FormProps>({
+    } = useForm<CreateUserFormData>({
+        resolver: zodResolver(createUserFormSchema),
         criteriaMode: 'all',
         mode: 'all',
-        resolver: zodResolver(schemaForm),
         defaultValues: {
             address: {
                 zipCode: '',
                 street: '',
                 number: '',
                 city: '',
-                state: ''
+                state: '',
+                district: ''
             }
         }
     });
 
     const zipCode = watch('address.zipCode');
 
-    const handleFormSubmit = (data: FormProps) => {
+    const handleFormSubmit = async (data: CreateUserFormData) => {
+        if (data.password !== data.confirmPassword) {
+            setError('confirmPassword', {
+                type: "manual",
+                message: "Senha e confirmação de senha não são iguais."
+            });
+            return;
+        }
         console.log(data);
+    
+        // Envio dos dados para a API
+        // try {
+        //     const response = await axios.post('URL_DA_SUA_API', data);
+        //     console.log(response.data);
+        //     // Você pode adicionar mais lógica aqui dependendo da resposta da sua API.
+        // } catch (error) {
+        //     console.error("Erro ao enviar dados para a API", error);
+        // }
     }
 
     const handleSetData = useCallback((data: AddressProps) => {
         setValue('address.city', data.localidade);
         setValue('address.street', data.logradouro);
         setValue('address.state', data.uf);
+        setValue('address.district', data.bairro);
     }, [])
 
     const handleFetchAddress = useCallback(async (zipCode: string) => {
@@ -104,10 +145,14 @@ export function RegisterPage() {
                                 Nome
                             </label>
                             <input 
-                            className="h-8 appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" 
-                            id="grid-first-name" 
-                            type="text">
+                                {...register('name')}
+                                className="h-8 appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" 
+                                id="grid-first-name" 
+                                type="text">
                             </input>
+                            {errors.name?.message && (
+                                <p className={styleP}>{errors.name?.message}</p>
+                            )}
                         </div>
 
                         <div className="w-full md:w-1/2 px-3">
@@ -115,10 +160,14 @@ export function RegisterPage() {
                                 Sobrenome
                             </label>
                             <input 
-                            className="h-8 appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" 
-                            id="grid-last-name" 
-                            type="text" >
+                                {...register('lastName')}
+                                className="h-8 appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" 
+                                id="grid-last-name" 
+                                type="text" >
                             </input>
+                            {errors.lastName?.message && (
+                                <p className={styleP}>{errors.lastName?.message}</p>
+                            )}
                         </div>
                     </div>
 
@@ -128,10 +177,48 @@ export function RegisterPage() {
                                 Email
                             </label>
                             <input 
-                            className="h-8 appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white "
-                             id="grid-email" 
-                             type="email" placeholder="Informe seu email">
+                                {...register('email')}
+                                className="h-8 appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white "
+                                id="grid-email" 
+                                type="email" placeholder="Informe seu email">
                             </input>
+                            {errors.email?.message && (
+                                <p className={styleP}>{errors.email?.message}</p>
+                            )}
+                        </div>
+                    </div>
+
+                    <div className="flex flex-wrap -mx-3 mb-4">
+                        <div className="w-full px-3">
+                            <label className="block text-gray-500 text-base mb-0">
+                                Senha
+                            </label>
+                            <input 
+                                {...register('password')}
+                                className="h-8 appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white "
+                                id="grid-password" 
+                                type="password" placeholder="***************">
+                            </input>
+                            {errors.password?.message && (
+                                <p className={styleP}>{errors.password?.message}</p>
+                            )}
+                        </div>
+                    </div>
+
+                    <div className="flex flex-wrap -mx-3 mb-4">
+                        <div className="w-full px-3">
+                            <label className="block text-gray-500 text-base mb-0">
+                                Confirme sua senha
+                            </label>
+                            <input 
+                                {...register('confirmPassword')}
+                                className="h-8 appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white "
+                                id="grid-confirm-password" 
+                                type="password" placeholder="***************">
+                            </input>
+                            {errors.confirmPassword?.message && (
+                                <p className={styleP}>{errors.confirmPassword?.message}</p>
+                            )}
                         </div>
                     </div>
                     
@@ -141,11 +228,14 @@ export function RegisterPage() {
                                 Telefone
                             </label>
                             <input 
-                            className="h-8 appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" 
-                            id="grid-phone" 
-                            type="tel">
+                                {...register('phone')}
+                                className="h-8 appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" 
+                                id="grid-phone" 
+                                type="tel">
                             </input>
-                            
+                            {errors.phone?.message && (
+                                <p className={styleP}>{errors.phone?.message}</p>
+                            )}
                         </div>
 
                         <div className="w-full md:w-1/2 px-3">
@@ -153,10 +243,14 @@ export function RegisterPage() {
                                 Data de Nascimento
                             </label>
                             <input 
-                            className="h-8 appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" 
-                            id="grid-birthday" 
-                            type="date" >
+                                {...register('birthdate')}
+                                className="h-8 appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" 
+                                id="grid-birthday" 
+                                type="date" >
                             </input>
+                            {errors.birthdate?.message && (
+                                <p className={styleP}>{errors.birthdate?.message}</p>
+                            )}
                         </div>
                     </div>
 
@@ -166,11 +260,14 @@ export function RegisterPage() {
                                 Username
                             </label>
                             <input 
-                            className="h-8 appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" 
-                            id="grid-username" 
-                            type="text">
+                                {...register('username')}
+                                className="h-8 appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" 
+                                id="grid-username" 
+                                type="text">
                             </input>
-                            
+                            {errors.username?.message && (
+                                <p className={styleP}>{errors.username?.message}</p>
+                            )}
                         </div>
 
                         <div className="w-full md:w-1/2 px-3">
@@ -178,30 +275,50 @@ export function RegisterPage() {
                                 CPF/CNPJ
                             </label>
                             <input 
-                            className="h-8 appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" 
-                            id="grid-cpf" 
-                            type="text" >
+                                {...register('cpf')}
+                                maxLength={11}
+                                className="h-8 appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" 
+                                id="grid-cpf" 
+                                type="text" >
                             </input>
+                            {errors.cpf?.message && (
+                                <p className={styleP}>{errors.cpf?.message}</p>
+                            )}
                         </div>
                     </div>
 
                     <div className="flex flex-wrap -mx-3 mb-4">
-                        <div className="w-full px-3">
+                        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                             <label className="block text-gray-500 text-base mb-0">
                                 CEP
                             </label>
                             <input 
-                            {...register('address.zipCode')}
-                            maxLength={9}
-                            className="h-8 appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" 
-                            id="grid-cep" 
-                            type="text" 
-                            placeholder="Informe seu CEP">
+                                {...register('address.zipCode')}
+                                maxLength={9}
+                                className="h-8 appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" 
+                                id="grid-cep" 
+                                type="text" 
+                                placeholder="Informe seu CEP">
                             </input>
                             {errors.address?.zipCode?.message && (
                                 <p className={styleP}>{errors.address?.zipCode?.message}</p>
                             )}
                         </div>
+                        <div className="w-full md:w-1/2 px-3">
+                            <label className="block text-gray-500 text-base mb-0">
+                                Estado
+                            </label>
+                            <input  
+                            {...register('address.state')}
+                            className="h-8 appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" 
+                            id="grid-state" 
+                            type="text">
+                            </input>
+                            {errors.address?.state?.message && (
+                                <p className={styleP}>{errors.address?.state?.message}</p>
+                            )}
+                        </div>
+
                     </div>
 
                     <div className="flex flex-wrap -mx-3 mb-4">
@@ -228,7 +345,7 @@ export function RegisterPage() {
                             <input 
                             className="h-8 appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" 
                             id="grid-number" 
-                            type="text">
+                            type="number">
                             </input>
                             {errors.address?.number?.message && (
                                 <p className={styleP}>{errors.address?.number?.message}</p>
@@ -237,7 +354,7 @@ export function RegisterPage() {
                     </div>
 
                     <div className="flex flex-wrap -mx-3 mb-5">
-                        <div className="w-full md:w-2/3 px-3 mb-6 md:mb-0">
+                        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                             <label className="block text-gray-500 text-base mb-0">
                                 Cidade
                             </label>
@@ -252,25 +369,25 @@ export function RegisterPage() {
                             )}
                         </div>
 
-                        <div className="w-full md:w-1/3 px-3">
+                        <div className="w-full md:w-1/2 px-3">
                             <label className="block text-gray-500 text-base mb-0">
-                                Estado
+                                Bairro
                             </label>
-                            <input 
-                            {...register('address.state')}
+                            <input  
+                            {...register('address.district')}
                             className="h-8 appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" 
-                            id="grid-state" 
+                            id="grid-district" 
                             type="text">
                             </input>
-                            {errors.address?.state?.message && (
-                                <p className={styleP}>{errors.address?.state?.message}</p>
+                            {errors.address?.district?.message && (
+                                <p className={styleP}>{errors.address?.district?.message}</p>
                             )}
                         </div>
                     </div>
                     
                     <div className="px-3 flex flex-wrap -mx-3 mb-2 w-full">
                         <button 
-                            type='submit'
+                            type="submit"
                             className='bg-jays-orange text-white font-semibold h-8 rounded w-1/3'>
                             Cadastrar
                         </button>

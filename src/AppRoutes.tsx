6f71ba45840f48b3edaec252b5b7895380c81ec9
OPaje <jecssilva@gmail.com>
2023-10-08 11:05:49 -0300

    Sprint 02 - tela nova senha

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -2,16 +2,17 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { LoginPage } from "./pages/LoginPage";
 import { RegisterPage } from "./pages/RegisterPage";
 import { SendRedefinePass } from "./pages/SendRedefinePass";
+import { NewPasswordPage } from "./pages/NewPasswordPage";
 
 export const AppRoutes = () => {
-    return (
-
-        <Router>
-            <Routes>
-                <Route path="/" element={<LoginPage/>}/>
-                <Route path="/cadastro" element={<RegisterPage/>}/>
-                <Route path="/redefinirsenha" element={<SendRedefinePass/>}/>
-            </Routes>
-        </Router>
-    )
-}
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/cadastro" element={<RegisterPage />} />
+        <Route path="/redefinirsenha" element={<SendRedefinePass />} />
+        <Route path="/novasenha" element={<NewPasswordPage />} />
+      </Routes>
+    </Router>
+  );
+};

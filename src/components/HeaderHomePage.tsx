a239d6d2d2f614287b1cacabb6703ac78ea83173
jpauloads <102815481+jpauloads@users.noreply.github.com>
2023-12-06 16:15:54 -0300

    Algumas coisinhas

--- src/components/HeaderHomePage.tsx
+++ src/components/HeaderHomePage.tsx
@@ -1,74 +1,106 @@
 // import React from "react";
-import jayslogo from "../assets/images/jayslogo.png";
 import { z } from "zod";
-import { Link, useNavigate } from 'react-router-dom';
-import { useContext } from 'react';
-import { AuthContext } from '../contexts/AuthContext'; // Ajuste o caminho conforme necessário
-import { api } from '../lib/axios';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import searchbutton from "../assets/images/SearchButtonBar.png";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../contexts/AuthContext"; // Ajuste o caminho conforme necessário
+import { api } from "../lib/axios";
 
 const loginFormSchema = z.object({
-    pesquisaPorNome: z.string()
+  pesquisaPorNome: z.string(),
 });
 
 type LoginFormInput = z.infer<typeof loginFormSchema>;
 
 export function Header() {
-    // const {
-    //     register,
-    //     handleSubmit
-    // } = useForm<LoginFormInput>({
-    //     resolver: zodResolver(loginFormSchema),
-    // });
+  const location = useLocation();
+  // const {
+  //     register,
+  //     handleSubmit
+  // } = useForm<LoginFormInput>({
+  //     resolver: zodResolver(loginFormSchema),
+  // });
 
-    const handleFormSubmit = async (data: LoginFormInput) => {
-        try {
-            console.log(data.pesquisaPorNome)
-            const response = await api.get(`/servico/buscaserviconome/${data.pesquisaPorNome}`);
-            console.log(response.data)
-        } catch (error) {
-            console.error("Erro na chamada da API", error);
-        }
-    };
-
-    const { user, logout } = useContext(AuthContext);
-    const navigate = useNavigate();
-
-    const handleLogout = () => {
-        logout();
-        navigate('/');
+  const handleFormSubmit = async (data: LoginFormInput) => {
+    try {
+      console.log(data.pesquisaPorNome);
+      const response = await api.get(
+        `/servico/buscaserviconome/${data.pesquisaPorNome}`
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.error("Erro na chamada da API", error);
     }
+  };
 
-    return (
-        <>
-            <div className="bg-jays-orange p-5">
-                <nav className="flex items-center justify-between">
-                    <h1 className="text-white">Jays</h1>
-                    <ul className="flex justify-center items-center text-white">
-                        <li className="pr-3">
-                            <button><Link to="/home">Início</Link></button>
-                        </li>
-                        <li className="pr-3">
-                            <button><Link to="#">Sobre nós</Link></button>
-                        </li>
-                        <li className="pr-3">
-                            {user ? (
-                                <button
-                                    onClick={handleLogout}
-                                    className="hover:bg-jays-hover transition-bg duration-300 text-white font-bold py-2 px-4 rounded border border-white-500">
-                                    Logout
-                                </button>
-                            ) : (
-                                <button className="hover:bg-jays-hover transition-bg duration-300 text-white font-bold py-2 px-4 rounded border border-white-500">
-                                    <Link to="/login" className="font-semibold">Login</Link>
-                                </button>
-                            )}
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </>
-    )
+  const { user, logout } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate("/");
+  };
+
+  return (
+    <>
+      <div className="bg-jays-orange p-5">
+        <nav className="flex items-center justify-between">
+          <h1 className="text-white">Jays</h1>
+          <ul className="flex justify-center items-center text-white">
+            <li className="pr-3">
+              <Link
+                to="/home"
+                className={
+                  location.pathname.startsWith("/home")
+                    ? "link-button text-orange-300"
+                    : "link-button"
+                }
+              >
+                Início
+              </Link>
+            </li>
+            <li className="pr-3 col" >
+            <Link
+                to="/servicos"
+                className={
+                  location.pathname.startsWith("/servicos")
+                    ? "link-button text-orange-300"
+                    : "link-button"
+                }
+              >
+                Serviços
+              </Link>
+            </li>
+            <li className="pr-3">
+            <Link
+                to="#"
+                className={
+                  location.pathname.startsWith("#")
+                    ? "link-button text-orange-300"
+                    : "link-button"
+                }
+              >
+                Sobre nós
+              </Link>
+            </li>
+            <li className="pr-3">
+              {user ? (
+                <button
+                  onClick={handleLogout}
+                  className="hover:bg-jays-hover transition-bg duration-300 text-white font-bold py-2 px-4 rounded border border-white-500"
+                >
+                  Logout
+                </button>
+              ) : (
+                <button className="hover:bg-jays-hover transition-bg duration-300 text-white font-bold py-2 px-4 rounded border border-white-500">
+                  <Link to="/login" className="font-semibold">
+                    Login
+                  </Link>
+                </button>
+              )}
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
 }
